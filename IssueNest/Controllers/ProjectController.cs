06b6c0d29b6868780e65a80a0e03496b840322a8
Rami Hikmat <ramihikmat10@gmail.com>
2020-07-09 14:02:59 +1000

    Primitive Project controller

---
+++ IssueNest/Controllers/ProjectController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using IssueNest.Data;
+using IssueNest.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IssueNest.Controllers
+{
+    [Route("api/project")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+
+        private IssuesDBContext db;
+        public ProjectController(IssuesDBContext db)
+        {
+            this.db = db;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProject()
+        {
+            if (Request.Headers.ContainsKey("user_id") && Request.Headers.ContainsKey("name"))
+            {
+                Request.Headers.TryGetValue("user_id", out var _userId);
+                Request.Headers.TryGetValue("name", out var name);
+
+                // Validate an int
+                if (!Int32.TryParse(_userId, out int userId))
+                    return BadRequest();
+
+                // Check if the user exists
+                if (await db.Users.FindAsync(userId) == null)
+                    return Unauthorized();
+
+                // Make a new project
+                await db.Projects.AddAsync(new Project
+                {
+                    UserId = userId,
+                    Name = name,
+                    Hook = Guid.NewGuid().ToString(),
+                });
+                await db.SaveChangesAsync();
+
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+    }
+}
--- IssueNest/Models/Project.cs
+++ IssueNest/Models/Project.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace IssueNest.Models
@@ -16,6 +17,8 @@ namespace IssueNest.Models
         [NotNull]
         public int UserId { get; set; }
         public User User { get; set; }
+        [NotNull]
+        public string Name { get; set; }
 
         [NotNull]
         public string Hook { get; set; }
