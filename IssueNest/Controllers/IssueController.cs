b4544fa080068eb3e8e6b0a7de0d15345c899f89
Rami Hikmat <ramihikmat10@gmail.com>
2020-07-11 13:59:22 +1000

    Get issue by project and id

--- IssueNest/Controllers/IssueController.cs
+++ IssueNest/Controllers/IssueController.cs
@@ -27,6 +27,18 @@ namespace IssueNest.Controllers
             //this.manager = manager;
         }
 
+        [HttpGet("{projectId}/{issueId}")]
+        public async Task<IActionResult> GetIssue(int projectId, int issueId)
+        {
+            Issue issue = await db.Issues.FirstOrDefaultAsync(p => p.ProjectId == projectId && p.Id == issueId);
+            if (issue != null)
+            {
+                return Ok(issue);
+            }
+
+            return NotFound();
+        }
+
         [HttpGet("{projectId}")]
         public async Task<IActionResult> GetIssues(int projectId)
         {
@@ -36,63 +48,6 @@ namespace IssueNest.Controllers
             List<Issue> list = await db.Issues.Where(p => p.ProjectId == projectId)
                 .ToListAsync();
             return Ok(list);
-        }
-
-        [HttpPost("{projectId}")]
-        public async Task<IActionResult> CreateIssue(int projectId)
-        {
-            if (await db.Projects.FindAsync(projectId) == null)
-                return NotFound();
-
-            try
-            {
-                //Request.Headers.TryGetValue("from", out var _from); // NOT IMPORTANT FOR NOW
-                Request.Headers.TryGetValue("description", out var description);
-                Request.Headers.TryGetValue("type", out var _type);
-
-                Enum.TryParse(_type.ToString().ToUpper(), out IssueType type);
-
-                await db.Issues.AddAsync(new Issue
-                {
-                    ProjectId = projectId,
-                    Description = description.ToString(),
-                    IssueFrom = IssueFrom.GITHUB.ToString(),
-                    IssueState = IssueState.EXISTING.ToString(),
-                    IssueType = type.ToString(),
-                });
-                await db.SaveChangesAsync();
-
-                return Ok();
-            } catch (ArgumentException)
-            {
-                return BadRequest();
-            }
-        }
-
-        // TODO MAKE THIS WORK
-        [HttpPatch("{id}")]
-        public async Task<IActionResult> UpdateIssue(int id)
-        {
-            try
-            {
-                Request.Headers.TryGetValue("state", out var _state);
-                // Check if valid state
-                Enum.TryParse(_state.ToString().ToUpper(), out IssueState state);
-
-                //Issue issue = new Issue { Id = id, IssueState = state.ToString() };
-                Issue issue = await db.Issues.FirstOrDefaultAsync(p => p.Id == id);
-                if (issue != null)
-                {
-                    issue.IssueState = state.ToString();
-                    await db.SaveChangesAsync();
-                    return Ok();
-                }
-                
-                return NotFound();
-            } catch (ArgumentException)
-            {
-                return BadRequest();
-            }
         }
     }
 }
